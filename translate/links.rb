a7830c0774ed8e3688482cb55835adca29560ea3
jiangyuezhang@yahoo.com.cn <jiangyuezhang@yahoo.com.cn>
2012-09-04 16:56:15 +0800

    translate complete

--- translate/links.rb
+++ translate/links.rb
@@ -2,14 +2,15 @@ require "spidr"
 require "nokogiri"
 require "open-uri"
 
+ url = "http://portable-crusher.com"
  links = Set[]
- Spidr.site("http://crusherstone.com") do |site|
+ Spidr.site(url) do |site|
  	site.every_html_page do |page|
  		links << page.url
  	end
  end
   		fi = File.open("links","a")
   		links.each do |link|
-           fi.puts(link)
+  		   fi.puts(link)
         end
         fi.close
