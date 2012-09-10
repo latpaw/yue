4e7347370d6b72ada2b7d9ca5f2f04cc14e26476
jiangyuezhang@yahoo.com.cn <jiangyuezhang@yahoo.com.cn>
2012-09-10 10:02:24 +0800

    css以及页面获取全部图片部分完成

--- translate/image.rb
+++ translate/image.rb
@@ -1,10 +1,22 @@
+# Function: Get all the images from a site, whatever in the page or css
+# Author: Emerson
+# Author URL: http://www.latpaw.me
+#
+#
+#
+# Help:
+# You must save the site and all the page url in a file named "links". The script will try to read the file and fetch all the images in the site. Cooperate with "links.rb"
+# and just run :
+# ruby imager.rb
+
+
 require 'nokogiri'
 require 'open-uri'
 require 'mechanize'
 require 'fileutils'
 require 'set'
 
-def get_image_css(uri,set)
+def get_image_css(uri,set)  # parse the html,get all the image and css
     a = Mechanize.new do |agent|
       agent.user_agent_alias = 'Mac Safari'
       # agent.set_proxy("172.16.2.139",8087)
@@ -23,7 +35,7 @@ def get_image_css(uri,set)
     return set
 end
 
-def fetch(uri,imgsrc,set)
+def fetch(uri,imgsrc,set) # process the image url to a absolute address, and return them in a set
            split_uri = uri.split("/")
            path = imgsrc.split("/") 
            if path[0] != "http" && path[0] != "" #if relative path,and not /
@@ -44,13 +56,7 @@ def fetch(uri,imgsrc,set)
            return set << imgsrc
 end
 
-set = Set[]
-IO.foreach("links") do |line|
-  line = line.chop
-  get_image_css(line,set)
-end
-
-
+def download(set) # download all image url in a set
   set.each do |imgsrc|
        path = imgsrc.split("/")
        img = open(imgsrc) {|f| f.read}
@@ -68,13 +74,31 @@ end
        end
        open("img/"+newpath+filename,"wb") {|f| f.write(img)}
   end
+end
 
-  # set.each do |link|
-  #   if link.index("css")
-  #       csspath = link.split("/")
-  #       csspath = csspath[3..csspath.length-1]
-  #       csspath = img+"/"+csspath.join("/")
-  #       fh = File.open(csspath,"r")
+def get_image_from_css(set,newset) #get images in css
+  set.each do |link|  
+    if link.index("css")
+        csspath = link.split("/")
+        csspath = csspath[3..csspath.length-1]
+        csspath = "img"+"/"+csspath.join("/")
+        fh = File.read(csspath)
+        result = fh.scan(/\([\S\s]+?\)/)
+        result.each do |img|
+          imgsrc = img[1..img.length-2]
+          fetch(link,imgsrc,newset)
+        end
+        return newset
+    end
+  end
+end
 
-  #   end
-  # end
+set = Set[]
+newset =Set[]
+IO.foreach("links") do |line|
+  line = line.chop
+  get_image_css(line,set)
+  download(set)
+  get_image_from_css(set,newset)
+  download(newset)
+end
