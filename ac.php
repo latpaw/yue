8a04bc1a854d789da1a148838fb7b2270905a916
jyz19880823 <jiangyuezhang@yahoo.com.cn>
2013-01-30 17:44:32 +0800

    2013-1-30

--- ac.php
+++ ac.php
@@ -1,13 +1,5 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-<body>
-	<section>
-		hello asd
-	</section>
-	<video src="132.flv"></video>
-</body>
-</html>
+<?php 
+
+echo isset($a)? $a:"no";
+
+?>
