98bc7189481c53183ca4671af962e5730cd17431
jyz19880823 <jiangyuezhang@yahoo.com.cn>
2013-02-01 17:34:25 +0800

    add mobile detect files

--- ac.php
+++ ac.php
@@ -1,5 +1,15 @@
-<?php 
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<script type="text/javascript">
+document.writeln(navigator.appVersion+"<br>")
+document.writeln(navigator.userAgent)
+	</script>
 
-echo isset($a)? $a:"no";
-
-?>
+	<div id="parameter" sid="0" pid="mtm" lid="fr" cid="zenith"></div><script type="text/javascript" src="http://js.sbmchina.com/xhr/xhr.js"></script>
+</body>
+</html>
