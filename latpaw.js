91d7ef29220b054f703025bc2edd89a04346ce31
dhh <dhh@latpaw.(none)>
2013-02-25 23:57:45 +0800

    old

--- latpaw.js
+++ latpaw.js
@@ -1,9 +1,6 @@
 //latpaw custom js frame and comman js snippets
 (function(){
 
-var latpaw = {}
+window.byid = function(id){return document.getElementById(id)}
 
-latpaw.byid = function(id){return document.getElementById(id)}
-
-window.latpaw =  latpaw
 })()
