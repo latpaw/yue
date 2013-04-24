0fdaee33bd8cc88f908c7681a329bb2837be103d
jyz19880823 <jiangyuezhang@yahoo.com.cn>
2013-04-24 17:46:44 +0800

    index create

--- xhrpost/test.js
+++ xhrpost/test.js
@@ -1,1 +1,5 @@
-document.write('<p id="ifr" lan="en" demo="1"></p><script type="text/javascript" src="http://latpaw.org:8081/xhrpost/cookie.js"></script>');
+document.write('<p id="ifr" lan="en" demo="1"></p><script type="text/javascript" src="http://inquiry.sbmchina.com/xhrpost/cookie.js"></script>');
+window.onload=function(){
+document.getElementById("ifr_inquiry").style.width="800px"
+document.getElementById("ifr_inquiry").style.height="567px"
+}
